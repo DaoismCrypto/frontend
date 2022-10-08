2e68f9b4c20af65a9fe536b2d28558391055b6b6
woowenjun99 <woowenjun99@gmail.com>
2022-10-08 16:16:23 +0800

    Add in the text field for numbers

--- src/components/BalanceCard.tsx
+++ src/components/BalanceCard.tsx
@@ -1,8 +1,9 @@
-import { Button, Card, CardActions, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
+import { Button, Card, CardActions, TextField, Stack, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
 import { Login, Logout } from '@mui/icons-material'
 import { MouseEvent, useState } from 'react'
 
-const ToggleBuySell = () => {
+// Used to toggle the buy-sell interface
+const Toggle = ({ options }: { options: string[] }) => {
   const [type, setType] = useState('buy')
 
   function handleChange(e: MouseEvent<HTMLElement>, value: string) {
@@ -10,15 +11,26 @@ const ToggleBuySell = () => {
     setType(value)
   }
 
+  const toggleButtons = options.map(option => {
+    return (
+      <ToggleButton key={option} fullWidth value={option}>
+        {option}
+      </ToggleButton>
+    )
+  })
+
   return (
-    <ToggleButtonGroup exclusive onChange={handleChange} value={type}>
-      <ToggleButton value='buy'>Buy</ToggleButton>
-      <ToggleButton value='sell'>Sell</ToggleButton>
-    </ToggleButtonGroup>
+    <Stack marginLeft='1rem' marginRight='1rem' marginBottom='1rem'>
+      <ToggleButtonGroup exclusive onChange={handleChange} fullWidth value={type}>
+        {toggleButtons}
+      </ToggleButtonGroup>
+    </Stack>
   )
 }
 
 const BalanceCard = ({ amount }: { amount: number }) => {
+  const [weight, setWeight] = useState(0)
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <Stack alignItems='center' paddingTop={2} width='100%'>
@@ -34,7 +46,26 @@ const BalanceCard = ({ amount }: { amount: number }) => {
           </Button>
         </Stack>
       </CardActions>
-      <ToggleBuySell />
+      <Toggle options={['buy', 'sell']} />
+      <Toggle options={['order', 'limit', 'stop']} />
+      <Stack width='100%'>
+        <TextField
+          onChange={e => {
+            e.preventDefault()
+            if (e.target.value === '-1') {
+              e.target.value = '0'
+            }
+            setWeight(parseInt(e.target.value))
+          }}
+          style={{
+            marginLeft: '1rem',
+            marginRight: '1rem',
+            marginBottom: '1rem'
+          }}
+          type='number'
+          value={weight}
+        />
+      </Stack>
     </Card>
   )
 }
